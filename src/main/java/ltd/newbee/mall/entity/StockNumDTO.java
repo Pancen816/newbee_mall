9d7de81f6a822e2a51e5ab793ae41a2fb0df64b0
bruce <brucer_cp@126.com>
2020-07-15 23:23:25 +0800

    提交newbee_mall项目

--- src/main/java/ltd/newbee/mall/entity/StockNumDTO.java
+++ src/main/java/ltd/newbee/mall/entity/StockNumDTO.java
@@ -12,19 +12,5 @@ public class StockNumDTO {
 
     private Integer goodsCount;
 
-   /* public Long getGoodsId() {
-        return goodsId;
-    }
 
-    public void setGoodsId(Long goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public Integer getGoodsCount() {
-        return goodsCount;
-    }
-
-    public void setGoodsCount(Integer goodsCount) {
-        this.goodsCount = goodsCount;
-    }*/
 }
